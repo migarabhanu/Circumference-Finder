8c1e01e2b46af37d9f3b434f4d7c5675ec5f6b24
Migara Bhanu <76873393+migarabhanu@users.noreply.github.com>
2021-08-22 18:41:13 +0530

    Files added

---
+++ C09.py
@@ -0,0 +1,22 @@
+#By Migara Bhanu
+#For BITS'21 Intra-School ICT Competition
+
+print("Welcome to Circumferance Finder !")
+print("For BITS'21 Intra-School Competetion")
+print("From Migara Bhanu")
+print("")#Just for a space
+
+pi=3.14
+r=int(input("Please enter the radius from Centimeters. But Don't enter the unit ? ONLY the NUMBER ? "))
+
+a=("Circumferance is ")
+b=(2*pi*r)
+c=(" cm")
+
+
+
+print(('{}{}{}'.format(a, b, c)))
+
+print("")#Just for a space
+print("")#Jusr for a sapce
+input("Thank you! Please press enter key to Exit !")
